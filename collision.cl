cb071d3ffcc581688e9258d4d625b847cd03818d
Kai Wohlfahrt <kai.scorpio@gmail.com>
2016-11-09 21:18:57 +0000

    Add initial collision detection
    Again, need wrapping and integrating with sorting

---
+++ collision.cl
@@ -0,0 +1,225 @@
+// https://devblogs.nvidia.com/parallelforall/thinking-parallel-part-i-collision-detection-gpu/
+
+// Interleaves bits with 2 zero bits
+unsigned int expandBits(unsigned int v) {
+    v = (v * 0x00010001u) & 0xFF0000FFu;
+    v = (v * 0x00000101u) & 0x0F00F00Fu;
+    v = (v * 0x00000011u) & 0xC30C30C3u;
+    v = (v * 0x00000005u) & 0x49249249u;
+    return v;
+}
+
+unsigned int morton(float3 pos, const float3 min, const float3 max) {
+    float3 norm_pos = (pos - min) / (max - min);
+    float3 pos_10bit = clamp(pos * 1023.0f, 0.0f, 1023.0f);
+
+    unsigned int xx = expandBits((unsigned int) pos_10bit.x);
+    unsigned int yy = expandBits((unsigned int) pos_10bit.y);
+    unsigned int zz = expandBits((unsigned int) pos_10bit.z);
+    return (xx << 2) + (yy << 1) + zz;
+}
+
+kernel void calculateCodes(global unsigned int * const codes,
+                           const global float * const coords,
+                           const global float * const range) {
+    const float3 min = vload3(0, range);
+    const float3 max = vload3(1, range);
+    codes[get_global_id(0)] = morton(vload3(get_global_id(0), coords), min, max);
+}
+
+char delta(const global unsigned int * const codes, const unsigned int n,
+                    const unsigned int idx, const long offset) {
+    if (offset < -((long) idx) || offset + idx >= n)
+        return -1;
+    return clz(codes[idx] ^ codes[idx + offset]);
+}
+
+char isign(const int x) {
+    return (x >= 0) * 2 - 1;
+}
+
+uint2 determineRange(const global unsigned int * const codes,
+                     const unsigned int n, const unsigned int idx) {
+    const char d = isign(delta(codes, n, idx, 1) - delta(codes, n, idx, -1));
+    const char delta_min = delta(codes, n, idx, -d);
+
+    // Find upper bound
+    unsigned int len_max = 1;
+    while (delta(codes, n, idx, ((long) len_max) * d) > delta_min)
+        len_max *= 2;
+
+    unsigned int len = 0;
+    unsigned int step = len_max / 2;
+
+    // Binary search
+    for (unsigned int step = len_max / 2; step > 0; step /= 2) {
+        long new_len = len + step;
+        if (delta(codes, n, idx, new_len*d) > delta_min)
+            len = new_len;
+    }
+
+    if (d > 0)
+        return (uint2)(idx, idx+len);
+    else
+        return (uint2)(idx-len, idx);
+}
+
+unsigned int findSplit(const global unsigned int * const codes, const uint2 range) {
+    const uint2 range_codes = (uint2)(codes[range.s0], codes[range.s1]);
+
+    if (range_codes.s0 == range_codes.s1)
+        // Split range in the middle
+        return (range.s0 + range.s1) / 2;
+
+    const unsigned char prefix = clz(range_codes.s0 ^ range_codes.s1);
+
+    // Initial guess
+    unsigned int split = range.s0;
+    unsigned int step = range.s1 - range.s0;
+
+    // Binary search
+    do {
+        step = (step + 1) / 2;
+        unsigned int new_split = split + step;
+
+        if (new_split < range.s1) {
+            unsigned char split_prefix = clz(range_codes.s0 ^ codes[new_split]);
+            if (split_prefix > prefix)
+                split = new_split;
+        }
+    } while (step > 1);
+
+    return split;
+}
+
+struct Node {
+    unsigned int parent;
+    union {
+        struct {
+            unsigned int id;
+        } leaf;
+        struct {
+            unsigned int children[2];
+        } internal;
+    };
+} __attribute__((packed));
+
+kernel void fillInternal(global struct Node * const nodes) {
+    const unsigned int n = get_global_size(0);
+    const size_t leaf_start = (n - 1);
+    nodes[leaf_start + get_global_id(0)].leaf.id = get_global_id(0);
+}
+
+kernel void generateBVH(const global unsigned int * const codes,
+                        global struct Node * const nodes) {
+    const unsigned int n = get_global_size(0) + 1;
+    const size_t idx = get_global_id(0);
+    const uint2 range = determineRange(codes, n, idx);
+    // (N-1) internal nodes + N leaf-nodes
+    const size_t leaf_start = n - 1;
+
+    unsigned int split = findSplit(codes, range);
+    const unsigned int child_a = (split == range.s0) ? leaf_start + split : split;
+    split += 1;
+    const unsigned int child_b = (split == range.s1) ? leaf_start + split : split;
+
+    nodes[idx].internal.children[0] = child_a;
+    nodes[idx].internal.children[1] = child_b;
+    nodes[child_a].parent = idx;
+    nodes[child_b].parent = idx;
+}
+
+struct Bound {
+    float min[3];
+    float max[3];
+} __attribute__((packed));
+
+kernel void generateBounds(global struct Bound * const bounds,
+                           global unsigned int * const flags,
+                           const global float * const coords,
+                           const global float * const radii,
+                           const global struct Node * const nodes) {
+    const unsigned int n = get_global_size(0);
+    const size_t D = 3;
+    const size_t leaf_start = n - 1;
+    size_t node_idx = leaf_start + get_global_id(0);
+    for (size_t d = 0; d < D; d++) {
+        size_t idx = nodes[node_idx].leaf.id;
+        bounds[node_idx].min[d] = coords[idx*D+d] - radii[idx];
+        bounds[node_idx].max[d] = coords[idx*D+d] + radii[idx];
+    }
+
+    do {
+        node_idx = nodes[node_idx].parent;
+        // Mark internal node as visited, and only process after children
+        if (atomic_inc(&flags[node_idx]) < 1)
+            break;
+        for (size_t d = 0; d < D; d++) {
+            const global unsigned int * child_idxs = nodes[node_idx].internal.children;
+            bounds[node_idx].min[d] = min(bounds[child_idxs[0]].min[d],
+                                          bounds[child_idxs[1]].min[d]);
+            bounds[node_idx].max[d] = max(bounds[child_idxs[0]].max[d],
+                                           bounds[child_idxs[1]].max[d]);
+        }
+    } while (node_idx != 0);
+}
+
+bool checkOverlap(const struct Bound a, const struct Bound b) {
+    const size_t D = 3;
+    bool collision = 1;
+    for (size_t d = 0; d < D; d++)
+        collision &= a.max[d] > b.min[d] && a.min[d] < b.max[d];
+    return collision;
+}
+
+bool isLeaf(const unsigned int idx, const unsigned int n) {
+    return idx >= (n - 1);
+}
+
+#pragma OPENCL EXTENSION cl_khr_int64_base_atomics : enable
+
+kernel void traverse(global unsigned int * const collisions,
+                     global unsigned long * const next,
+                     const unsigned long n_collisions,
+                     const global struct Node * const nodes,
+                     const global struct Bound * const bounds) {
+    unsigned int n = get_global_size(0);
+    size_t leaf_start = n - 1;
+    const struct Bound query = bounds[leaf_start + get_global_id(0)];
+
+    unsigned int stack[64];
+    unsigned char stack_ptr = 0;
+    stack[stack_ptr++] = UCHAR_MAX; // push NULL node (i.e. invalid node)
+
+    // Root node
+    unsigned int idx = 0;
+    do {
+        unsigned int child_a = nodes[idx].internal.children[0];
+        unsigned int child_b = nodes[idx].internal.children[1];
+        bool overlap_a = checkOverlap(query, bounds[child_a]);
+        bool overlap_b = checkOverlap(query, bounds[child_b]);
+        if (overlap_a && isLeaf(child_a, n)) {
+            unsigned long collision_idx = atom_inc(next);
+            if (collision_idx < n_collisions) {
+                collisions[collision_idx*2+0] = get_global_id(0);
+                collisions[collision_idx*2+1] = nodes[child_a].leaf.id;
+            }
+        }
+        if (overlap_b && isLeaf(child_b, n)) {
+            unsigned long collision_idx = atom_inc(next);
+            if (collision_idx < n_collisions) {
+                collisions[collision_idx*2+0] = get_global_id(0);
+                collisions[collision_idx*2+1] = nodes[child_a].leaf.id;
+            }
+        }
+        bool traverse_a = (overlap_a && !isLeaf(child_a, n));
+        bool traverse_b = (overlap_b && !isLeaf(child_b, n));
+        if (!traverse_a && !traverse_b)
+            idx = stack[--stack_ptr];
+        else {
+            idx = (traverse_a) ? child_a : child_b;
+            if (traverse_a && traverse_b)
+                stack[stack_ptr++] = child_b;
+        }
+    } while (idx != UCHAR_MAX);
+}
---
+++ tests/test_collision.py
@@ -0,0 +1,302 @@
+import numpy as np
+import pyopencl as cl
+from pathlib import Path
+import pytest
+from itertools import product as cartesian
+
+np.random.seed(4)
+
+kernel_args = {'generateBVH': None,
+               'fillInternal': None,
+               'generateBounds': None,
+               'calculateCodes': None,
+               'traverse': [None, None, np.dtype('uint64'), None, None],}
+Node = np.dtype([('parent', 'uint32'), ('data', 'uint32', 2)])
+
+@pytest.fixture(scope='module')
+
+def cl_kernels():
+    ctx = cl.create_some_context()
+    cq = cl.CommandQueue(ctx)
+
+    src = Path(__file__).parent / ".." / "collision.cl"
+    with src.open("r") as f:
+        program = cl.Program(ctx, f.read()).build()
+        kernels = {name: getattr(program, name) for name in kernel_args}
+        for name, kernel in kernels.items():
+            arg_types = kernel_args[name]
+            if arg_types is not None:
+                kernel.set_scalar_arg_dtypes(arg_types)
+
+    return ctx, cq, kernels
+
+
+def test_fill_internal(cl_kernels):
+    ctx, cq, kernels = cl_kernels
+
+    n = 8
+    nodes_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_WRITE, (2 * n - 1) * Node.itemsize
+    )
+
+    fill_internal = kernels['fillInternal'](
+        cq, (n,), None,
+        nodes_buf
+    )
+
+    (nodes_map, _) = cl.enqueue_map_buffer(
+        cq, nodes_buf, cl.map_flags.READ,
+        (n - 1) * Node.itemsize, n, Node,
+        wait_for=[fill_internal], is_blocking=True
+    )
+    nodes_map.dtype = Node
+
+    np.testing.assert_equal(nodes_map['data'][:, 0], np.arange(n, dtype='uint32'))
+
+
+def test_generate_bvh(cl_kernels):
+    ctx, cq, kernels = cl_kernels
+
+    # From Figure 3
+    codes = np.array([0b00001, 0b00010, 0b00100, 0b00101,
+                      0b10011, 0b11000, 0b11001, 0b11110], dtype='uint32')
+    n_nodes = len(codes) * 2 - 1
+
+    codes_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=codes
+    )
+    nodes_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_WRITE, n_nodes * Node.itemsize
+    )
+
+    fill_internal = kernels['fillInternal'](
+        cq, (len(codes),), None,
+        nodes_buf
+    )
+    generate_bvh = kernels['generateBVH'](
+        cq, (len(codes) - 1,), None,
+        codes_buf, nodes_buf,
+        wait_for=[fill_internal]
+    )
+    (nodes_map, _) = cl.enqueue_map_buffer(
+        cq, nodes_buf, cl.map_flags.READ,
+        0, n_nodes, Node,
+        wait_for=[generate_bvh], is_blocking=True
+    )
+    nodes_map.dtype = Node
+
+    leaf = len(codes) - 1
+    expected = np.array([(-1, [3, 4]),
+                         (3, [leaf+0, leaf+1]),
+                         (3, [leaf+2, leaf+3]),
+                         (0, [1, 2]),
+                         (0, [leaf+4, 5]),
+                         (4, [6, leaf+7]),
+                         (5, [leaf+5, leaf+6])], dtype=Node)
+    np.testing.assert_equal(nodes_map[1:leaf], expected[1:])
+    np.testing.assert_equal(nodes_map['data'][0], expected['data'][0])
+
+    expected_parents = np.array([1, 1, 2, 2, 4, 6, 6, 5], dtype='uint32')
+    np.testing.assert_equal(nodes_map['parent'][leaf:], expected_parents)
+    expected_ids = np.arange(len(codes), dtype='uint32')
+    np.testing.assert_equal(nodes_map['data'][leaf:, 0], np.arange(len(codes), dtype='uint32'))
+
+
+def test_generate_odd_bvh(cl_kernels):
+    ctx, cq, kernels = cl_kernels
+
+    # From Figure 3
+    codes = np.array([0b00001, 0b00010, 0b00100, 0b00101,
+                      0b10011, 0b11000, 0b11001], dtype='uint32')
+    n_nodes = len(codes) * 2 - 1
+
+    codes_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=codes
+    )
+    nodes_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_WRITE, n_nodes * Node.itemsize
+    )
+
+    fill_internal = kernels['fillInternal'](
+        cq, (len(codes),), None,
+        nodes_buf
+    )
+    generate_bvh = kernels['generateBVH'](
+        cq, (len(codes) - 1,), None,
+        codes_buf, nodes_buf,
+        wait_for=[fill_internal]
+    )
+    (nodes_map, _) = cl.enqueue_map_buffer(
+        cq, nodes_buf, cl.map_flags.READ,
+        0, n_nodes, Node,
+        wait_for=[generate_bvh], is_blocking=True
+    )
+    nodes_map.dtype = Node
+
+    leaf = len(codes) - 1
+    expected = np.array([(-1, [3, 4]),
+                         (3, [leaf+0, leaf+1]),
+                         (3, [leaf+2, leaf+3]),
+                         (0, [1, 2]),
+                         (0, [leaf+4, 5]),
+                         (4, [leaf+5, leaf+6])], dtype=Node)
+    np.testing.assert_equal(nodes_map[1:leaf], expected[1:])
+    np.testing.assert_equal(nodes_map['data'][0], expected['data'][0])
+
+    expected_parents = np.array([1, 1, 2, 2, 4, 5, 5], dtype='uint32')
+    np.testing.assert_equal(nodes_map['parent'][leaf:], expected_parents)
+    expected_ids = np.arange(len(codes), dtype='uint32')
+    np.testing.assert_equal(nodes_map['data'][leaf:, 0], np.arange(len(codes), dtype='uint32'))
+
+
+def test_compute_bounds(cl_kernels):
+    ctx, cq, kernels = cl_kernels
+
+    coords = np.array([[ 0.0, 1.0, 3.0],
+                       [ 4.0, 1.0, 8.0],
+                       [-4.0,-6.0, 3.0],
+                       [-5.0, 0.0,-1.0]], dtype='float32')
+    radii = np.ones(len(coords), dtype='float32')
+    leaf = len(coords) - 1
+    nodes = np.array([(-1, (leaf+0, 1)),
+                      ( 0, (leaf+3, 2)),
+                      ( 1, (leaf+1, leaf+2)),
+                      ( 0, (0, -1)),
+                      ( 2, (1, -1)),
+                      ( 2, (2, -1)),
+                      ( 1, (3, -1))], dtype=Node)
+
+    coords_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=coords
+    )
+    radii_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=radii
+    )
+    nodes_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=nodes
+    )
+    bounds_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_WRITE, len(nodes) * 3 * 2 * np.dtype('float32').itemsize
+    )
+    flags_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_WRITE, len(nodes) * np.dtype('uint32').itemsize
+    )
+
+    clear_flags = cl.enqueue_fill_buffer(
+        cq, flags_buf, np.zeros(1, dtype='uint32'),
+        0, len(nodes) * np.dtype('uint32').itemsize
+    )
+    calc_bounds = kernels['generateBounds'](
+        cq, (len(coords),), None,
+        bounds_buf, flags_buf, coords_buf, radii_buf, nodes_buf,
+        wait_for=[clear_flags]
+    )
+    (bounds_map, _) = cl.enqueue_map_buffer(
+        cq, bounds_buf, cl.map_flags.READ,
+        0, (len(nodes), 2, 3), np.dtype('float32'),
+        wait_for=[calc_bounds], is_blocking=True
+    )
+
+    expected = np.array([[[-6.0,-7.0,-2.0], [ 5.0, 2.0, 9.0]],
+                         [[-6.0,-7.0,-2.0], [ 5.0, 2.0, 9.0]],
+                         [[-5.0,-7.0, 2.0], [ 5.0, 2.0, 9.0]],
+                         [[-1.0, 0.0, 2.0], [ 1.0, 2.0, 4.0]],
+                         [[ 3.0, 0.0, 7.0], [ 5.0, 2.0, 9.0]],
+                         [[-5.0,-7.0, 2.0], [-3.0,-5.0, 4.0]],
+                         [[-6.0,-1.0,-2.0], [-4.0, 1.0, 0.0]]], dtype='float32')
+    np.testing.assert_equal(bounds_map, expected)
+
+
+def test_traverse(cl_kernels):
+    ctx, cq, kernels = cl_kernels
+
+    coords = np.array([[ 0.0, 1.0, 3.0],
+                       [ 4.0, 1.0, 8.0],
+                       [-4.0,-6.0, 3.0],
+                       [-5.0, 0.0,-1.0],
+                       [-5.0, 0.5,-0.5]], dtype='float32')
+    radii = np.ones(len(coords), dtype='float32')
+    n_nodes = len(coords) * 2 - 1
+
+    coords_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=coords
+    )
+    range_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR,
+        hostbuf=np.array([coords.min(axis=0), coords.max(axis=0)], dtype=coords.dtype)
+    )
+    codes_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_WRITE, len(coords) * np.dtype('uint32').itemsize
+    )
+    radii_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=radii
+    )
+    nodes_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_WRITE, n_nodes * Node.itemsize
+    )
+    bounds_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_WRITE, n_nodes * 3 * 2 * np.dtype('float32').itemsize
+    )
+    flags_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_WRITE, n_nodes * np.dtype('float32').itemsize
+    )
+    n_collisions = 2
+    collisions_buf = cl.Buffer(
+        ctx, cl.mem_flags.WRITE_ONLY, n_collisions * 2 * np.dtype('uint32').itemsize
+    )
+    n_collisions_buf = cl.Buffer(
+        ctx, cl.mem_flags.READ_WRITE, np.dtype('uint64').itemsize
+    )
+
+    calc_codes = kernels['calculateCodes'](
+        cq, (len(coords),), None,
+        codes_buf, coords_buf, range_buf,
+    )
+
+    # Would use radix sort
+    (codes_map, _) = cl.enqueue_map_buffer(
+        cq, codes_buf, cl.map_flags.READ | cl.map_flags.WRITE,
+        0, (len(coords),), np.dtype('uint32'),
+        wait_for=[calc_codes], is_blocking=True
+    )
+    codes_map[...] = np.sort(codes_map, kind='mergesort')
+    del codes_map
+    fill_internal = kernels['fillInternal'](
+        cq, (len(coords),), None,
+        nodes_buf
+    )
+    generate_bvh = kernels['generateBVH'](
+        cq, (len(coords)-1,), None,
+        codes_buf, nodes_buf,
+        wait_for=[calc_codes, fill_internal]
+    )
+    clear_flags = cl.enqueue_fill_buffer(
+        cq, flags_buf, np.zeros(1, dtype='uint32'),
+        0, n_nodes * np.dtype('uint32').itemsize
+    )
+    calc_bounds = kernels['generateBounds'](
+        cq, (len(coords),), None,
+        bounds_buf, flags_buf, coords_buf, radii_buf, nodes_buf,
+        wait_for=[clear_flags, generate_bvh]
+    )
+    clear_collisions = cl.enqueue_fill_buffer(
+        cq, collisions_buf, np.array([-1], dtype='uint32'),
+        0, n_collisions * 2 * np.dtype('uint32').itemsize
+    )
+    clear_n_collisions = cl.enqueue_fill_buffer(
+        cq, n_collisions_buf, np.zeros(1, dtype='uint64'),
+        0, np.dtype('uint64').itemsize
+    )
+    find_collisions = kernels['traverse'](
+        cq, (len(coords),), None,
+        collisions_buf, n_collisions_buf, n_collisions, nodes_buf, bounds_buf,
+        wait_for=[clear_collisions, clear_n_collisions, calc_bounds],
+        global_offset=(0,)
+    )
+
+    (n_collisions_map, _) = cl.enqueue_map_buffer(
+        cq, n_collisions_buf, cl.map_flags.READ,
+        0, 1, np.dtype('uint64'),
+        wait_for=[find_collisions], is_blocking=True
+    )
+    assert n_collisions_map[0] == 5 + 2
